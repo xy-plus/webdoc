37bc04c6d8c421ba3f9cd1ec809d195079be5672
刘丰源 <liufy17@mails.tsinghua.edu.cn>
2019-10-22 17:23:02 +0800

    update

--- docs/update_book.sh
+++
@@ -1,6 +0,0 @@
-rm -rf docs
-gitbook build
-mv _book docs
-git add .
-git commit -m "update"
-git push
--- update_book.sh
+++ update_book.sh
@@ -1,6 +1,9 @@
 rm -rf docs
 gitbook build
 mv _book docs
+cd docs
+rm update_book.sh
+cd ..
 git add .
 git commit -m "update"
 git push
