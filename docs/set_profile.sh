0d74a6cc02aafabb9a7d73d1f2e2a8dca463295d
刘丰源 <liufy17@mails.tsinghua.edu.cn>
2019-10-29 15:37:24 +0800

    update web

--- docs/set_profile.sh
+++ docs/set_profile.sh
@@ -1,3 +1,19 @@
+profile=$(cat profile.conf)
+origin_profile="clientID: 'GitHub Application Client ID',
+clientSecret: 'GitHub Application Client Secret',
+repo: 'GitHub repo',
+owner: 'GitHub repo owner',
+admin: [
+    'GitHub repo owner and collaborators, only these guys can initialize github issues'
+],
+createIssueManually: true,
+id: document.title"
+
+if [ "$profile" = "$origin_profile" ]
+then
+echo "Error: Please change porfile.conf !!!"
+else
+cat > gitbook/comment.html << EOF
 <link
   rel="stylesheet"
   href="https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.css"
@@ -6,26 +22,14 @@
 <div id="gitalk-container" style="margin: 5%"></div>
 <script>
   const gitalk = new Gitalk({
-clientID: '00c208c2cc4bae4c64cc',
-clientSecret: '3dd4511e0db4d8fa14616a395ccb3e05151032ed',
-repo: 'webdoc',
-owner: 'xy-plus',
-admin: ['xy-plus'],
-createIssueManually: true,
-id: document.title
+${profile}
   })
   gitalk.render('gitalk-container')
 </script>
 <script>
   require(['gitbook'], function(gitbook) {
     const gitalk = new Gitalk({
-clientID: '00c208c2cc4bae4c64cc',
-clientSecret: '3dd4511e0db4d8fa14616a395ccb3e05151032ed',
-repo: 'webdoc',
-owner: 'xy-plus',
-admin: ['xy-plus'],
-createIssueManually: true,
-id: document.title
+${profile}
     })
     gitbook.events.bind('page.change', initMygitalk)
     function initMygitalk() {
@@ -33,3 +37,7 @@ id: document.title
     }
   })
 </script>
+EOF
+echo "Done! run `gitbook serve` to preview your web doc"
+fi
+
